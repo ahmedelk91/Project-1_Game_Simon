6cd43bb11a12672c8d8d49e6c641326cad2216ca
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-11 15:02:46 -0400

    Changed format, added new variables

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 var colors = ["#red","#blue","#yellow","#green"]
+var simonMoves=[]
+
 $(document).ready(function(){
+
   console.log("hello")
-  var randomDivId = colors[Math.floor(Math.random()*colors.length)];
+
+  var randomDivId = Math.floor(Math.random()*colors.length);
+  simonMoves.push(randomDivId)
+  simonSays = function() {
+    for (i=0;i<simonMoves.length;i++){
+      $(colors[simonMoves[i]]).fadeOut("slow").fadeIn("slow");
+      console.log('I work!')
+    }
+  }
   $("#button").on("click", function(evt){
     evt.preventDefault();
-    console.log(randomDivId, "?");
-    $(randomDivId).fadeOut("slow");
-    $(randomDivId).fadeIn("slow");
-    setInterval(evt, 3000)
-    clearInterval(evt, 9000)
-    console.log("interval works");
-
-    function makeid()
-    {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-        for( var i=0; i < 5; i++ )
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        return text;
-    }
-
-
-    console.log("fade works")
-    console.log("button works")
-    console.log(randomDivId, "!")
-    // Math.random() returns a rational number between 0 and 1 up to 8 places
-      });
+    simonSays();
   });
 
+})
+//   for(i=0; i >= randomDivId; i++ )
+//   console.log(randomDivId, "?");
+//   $(randomDivId).fadeOut("slow");
+//   setInterval(evt, 3000)
+//   $(randomDivId).fadeIn("slow");
+//   clearInterval(evt, 9000)
+//   console.log("interval works");
+//
+//   console.log("fade works")
+//   console.log("button works")
+//   console.log(randomDivId, "!")
+//   // Math.random() returns a rational number between 0 and 1 up to 8 places
+//     });
+// });
 
-  //
-  // $("#blue").fadeOut("slow");
-  // $("#blue").fadeIn("slow");
-  //
-  // $("#green").fadeOut(3000);
-  // $("#green").fadeIn("slow");
-  //
-  // $("#yellow").fadeOut(3000);
-  // $("#yellow").fadeIn("slow");
+
+//
+// $("#blue").fadeOut("slow");
+// $("#blue").fadeIn("slow");
+//
+// $("#green").fadeOut(3000);
+// $("#green").fadeIn("slow");
+//
+// $("#yellow").fadeOut(3000);
+// $("#yellow").fadeIn("slow");
