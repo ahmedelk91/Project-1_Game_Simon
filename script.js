1ff6862045476ca27164527e4ebcc88acfc0fb64
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-12 15:05:17 -0400

    cleaned up comments, added more annotations

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
 $(document).ready(function(){
   console.log("hello")
-  //variable that generates random sequence
-  //divs that will be clicked by the player to respond
+
+  //player choices
   var colors = ["#red","#blue","#yellow","#green"]
-  //the randomly generated divs, recorded and pushed to this blank array
+  //simonMoves - empty array recording random sequences Simon spits out; clears out with the emptyArray function
   var simonMoves=[]
-  //the pattern the user creates, recorded and pushed to this blank array
+  //userMoves - records player choices
   var userMoves=[]
-  //the turn the player is on
-  // var turns = 1
-  // //players clicks to respond to simonMoves
-  // var clicks = 0
-  //gets the doc ready for jquery
+
+  //function that adds a number to the simonMoves array
   function addNumToArray() {
+  //randomDivId creates a random variable
   var randomDivId = Math.floor(Math.random()*colors.length);
-  //pushes the randomly generated sequence into the simonMoves array
+  //pushes the randomDivId to the simonMoves array
   simonMoves.push(colors[randomDivId])
+  //once the simonMoves array has 1 item, the start button is no longer functional
   if (simonMoves.length == 1){
     turnOffStart();
   }
+  //calls the simonSays function which animates a number being added to the array; the flash to indicate a move has been made
   simonSays();
   }
 
-  //console.log(colors[randomDivId])
   console.log(simonMoves)
-
-  //function that loops sequences
+  //function that adds a random value for the simonMoves array and animates these changes
   function simonSays() {
-    console.log("Length of loop : "+simonMoves.length);
+    // console.log("Length of loop : "+simonMoves.length);
     for (i=0;i<simonMoves.length;i++){
       $(simonMoves[i]).fadeOut("slow").fadeIn("slow");
       console.log('I work!')
     }
   }
 
+//start button; when clicked, starts a function that addNumToArray
   $("#button").on("click", function(evt){
     evt.preventDefault();
-    //simonSays()
     addNumToArray();
-
   });
+//turns off the start button so it only needs to be started once
   function turnOffStart(){
     $("#button").off("click");
   }
 
-
-  // $(".action").click(function() {
-  //   userMoves.push(color)
+//player choice buttons - function pushes click data to userMoves array
   $("#red").on("click", function() {
     console.log("Red works")
     userMoves.push(colors[0])
     console.log("Simon Array : "+simonMoves)
     console.log("User Array : " +userMoves)
+//animates the button the player clicks on
     $(this).fadeOut("slow").fadeIn("slow");
+//calls the checkWinner function which evaluates whether or not the player action matches the simon action
     checkWinner(userMoves, simonMoves)
   })
   $("#blue").on("click", function() {
@@ -77,112 +75,36 @@ $(document).ready(function(){
     checkWinner(userMoves, simonMoves)
   })
 
+  //evaluates whether or not the player action matches the simon action
   function checkWinner(userMoves, simonMoves){
-    //
+//calls the isEqual function which compares two arrays
     if (isEqual(userMoves, simonMoves)) {
+//compares the length of both arrays
       if (userMoves.length === simonMoves.length) {
-      emptyArray()
-      addNumToArray();
+//calls the emptyArray function which resets the userMoves array
+        emptyArray()
+//function that adds a number to the simonMoves array
+        addNumToArray();
       }
     } else {
       alert("LOSER!")
     }
   }
+
+  //function which allows us to compare two arrays
   function isEqual(array1, array2){
 
     var ret = true;
     for(var i = 0; i < array1.length; i++){
       console.log(array1[i], array2[i]);
       if(array1[i] != array2[i]) {
-      ret = false;
+        ret = false;
       }
     }
     return ret;
   }
+  //resets the userMoves array
   function emptyArray(array1) {
-  // if(isEqual == true) return simonMoves=[];
-  userMoves = [];
-}
+    userMoves = [];
+  }
 });
-
-
-
-//check if simonMoves and Usermoves are equal
-//if yes then call simonSays
-//if turns == clicks, then a for loop will run, adding an extra "color" to the sequence
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if(userMoves[i] === simonMoves[i]){
-//   console.log(colors[i])
-//   alert("good job!")
-// } else {
-//   alert("LOSER!")
-// }
-// console.log("working color button")
-
-//     // // $("#blue").on("click", function(){
-//       console.log("working blue button")
-//     // // })
-//     // // $("#green").on("click", function(){
-//       console.log("working green button")
-//     // // })
-//     // // $("#yellow").on("click", function(){
-//       console.log("working yellow button")
-//
-// // var playerInput =
-// // if(playerInput === simonSays) {
-//   prompt("Good job!")
-// }
-
-
-
-
-
-// //   for(i=0; i >= randomDivId; i++ )
-// // //   $(randomDivId).fadeOut("slow");
-// // //   setInterval(evt, 3000)
-// // //   $(randomDivId).fadeIn("slow");
-// // //   clearInterval(evt, 9000)
-//   console.log("interval works");
-// // //
-//   console.log("fade works")
-//   console.log("button works")
-// //   // Math.random() returns a rational number between 0 and 1 up to 8 places
-// //
-// //
-//
-
-//
-// $("#blue").fadeOut("slow");
-// $("#blue").fadeIn("slow");
-//
-// $("#green").fadeOut(3000);
-// $("#green").fadeIn("slow");
-//
-// $("#yellow").fadeOut(3000);
-// $("#yellow").fadeIn("slow");
